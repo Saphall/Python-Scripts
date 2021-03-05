63d710ebf98f4dda62651503a6b71e19ce736b93
Saphall <safalsakha@gmail.com>
2021-03-05 09:01:00 +0545

    commit

--- 2MacChanger/macChanger.py
+++ 2MacChanger/macChanger.py
@@ -1,10 +1,12 @@
 import subprocess
 
-interface = 'enp3s0'
-mac = '00:11:22:33:44:55'
+# interface = 'enp3s0'
+# mac = '00:11:22:33:44:55'
 
-interface = input('Enter interface to change: ')
-mac = input('Enter a valid mac adress: ')
+interface = input('interface > ')
+mac = input('new mac > ')
+
+print('[+] Changing MAC address for '+interface+' to '+mac)
 
 subprocess.call('sudo ifconfig ' + interface+' down', shell=True)
 subprocess.call('sudo ifconfig '+interface+' hw ether '+mac, shell=True)
