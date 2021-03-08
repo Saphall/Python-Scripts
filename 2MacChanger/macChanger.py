fe1f3ea14aa8ad8a02ef63c24ae8f0d9dde46a4c
Saphall <safalsakha@gmail.com>
2021-03-08 15:56:37 +0545

    commit

--- 2MacChanger/macChanger.py
+++ 2MacChanger/macChanger.py
@@ -1,13 +1,25 @@
 import subprocess
+import optparse
 
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--interface', dest='interface',
+                      help='Interface to change its MAC address.')
+    parser.add_option('-m', '--mac', dest='mac', help='New MAC address.')
+    return parser.parse_args()
+
+
+def mac_changer(interface, mac):
+    print('Changing MAC address for '+interface+' to '+mac+'.')
+    subprocess.call(['sudo', 'ifconfig', interface, 'down'])
+    subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', mac])
+    subprocess.call(['sudo', 'ifconfig', interface, 'up'])
+    print('Done!')
+
+
+# print(options)
 # interface = 'enp3s0'
 # mac = '00:11:22:33:44:55'
-
-interface = input('interface > ')
-mac = input('new mac > ')
-
-print('[+] Changing MAC address for '+interface+' to '+mac)
-
-subprocess.call('sudo ifconfig ' + interface+' down', shell=True)
-subprocess.call('sudo ifconfig '+interface+' hw ether '+mac, shell=True)
-subprocess.call('sudo ifconfig ' + interface + ' up', shell=True)
+(options, arguments) = get_arguments()
+mac_changer(options.interface, options.mac)
